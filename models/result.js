bc8425bb246e1e65de97b0045530c3791db331d4
oji <dave@startrekstud.com>
2017-02-18 03:45:34 -0800

    add result

---
+++ models/result.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+
+// Result Schema
+var resultSchema = mongoose.Schema({
+	result_names:{
+		type: String,
+        required: true
+	},
+	offense:{
+		type: String,
+        required: true
+	},
+	defense:{
+		type: String,
+        required: true
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Result = module.exports = mongoose.model('Result', resultSchema);
+
+// Get Results
+module.exports.getResults = function(callback, limit){
+	Result.find(callback).limit(limit);
+}
+
+// Get Result
+module.exports.getResultById = function(id, callback){
+	Result.findById(id, callback);
+}
+
+// Add Result
+module.exports.addResult = function(result, callback){
+	Result.create(result, callback);
+}
+
+// Update Result
+module.exports.updateResult = function(id, result, options, callback){
+	var query = {_id: id};
+	var update = {
+		schedules: result.schedules,
+		first_resulters: result.first_resulters,
+		second_resulters: result.second_resulters,
+		results: result.results
+	}
+	Result.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Result
+module.exports.removeResult = function(id, callback){
+	var query = {_id: id};
+	Result.remove(query, callback);
+}
