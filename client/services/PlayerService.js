eb96407172b37e6ac5fcca437d100820d3a87944
SethVg <Savggamer@gmail.com>
2017-03-19 18:03:29 -0700

    Tryna somethin

--- client/services/PlayerService.js
+++ client/services/PlayerService.js
@@ -1,23 +1,51 @@
-myApp.service('PlayerService', function(){
+myApp.service('PlayerService', function () {
 
     this.players = [];
     this.plays = [];
 
-    this.getSetPlayers = function(val){
-        if(!!val){
+    this.getSetPlayers = function (val) {
+        if (!!val) {
             this.players = val;
-        }else{
+        } else {
             return this.players;
         }
     }
 
 
-    this.getSetPlays = function(val){
-        if(!!val){
+    this.getSetPlays = function (val) {
+
+        if (!!val) {
             this.plays = val;
-        }else{
+            populatePlays();
+        } else {
             return this.plays;
         }
+    }
+
+    function populatePlays() {
+        for (var a = 0; a < this.players.length; a++) {
+            var player = this.players[a];
+            player.catches = 0;
+        }
+
+        for (var a = 0; a < this.plays.length; a++) {
+            var play = this.plays[a];
+
+            if(play.receiver_results === "Catch"){
+                getPlayerByName(play.receivers.catches++);
+            }
+        }
+    }
+
+    function getPlayerByName(str) {
+        for (var a = 0; a < this.players.length; a++) {
+            var player = this.players[a];
+            if (player.full_names === str) {
+                return player;
+            }
+        }
+
+        return null;
     }
 
     return this;
