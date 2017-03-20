c6df98874e33e7482fdce61f3f7cd0257add11a8
SethVg <Savggamer@gmail.com>
2017-03-19 17:38:47 -0700

    Bye bye bye

--- client/controllers/players.js
+++ client/controllers/players.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('PlayersController', ['$scope', '$http', '$location', '$routeParams', 'PlayerService', function($scope, $http, $location, $routeParams, playerService){
+myApp.controller('PlayersController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 	console.log('PlayersController loaded...');
 
     $scope.players = null;
@@ -12,7 +12,7 @@ myApp.controller('PlayersController', ['$scope', '$http', '$location', '$routePa
             data: { applicationId: 3 }
         }).success(function (response) {
         $scope.playerList = response;
-        this.playerService.getSetPlayers(response);
+        playerService.getSetPlayers(response);
     });
 
     $scope.teams = null;
