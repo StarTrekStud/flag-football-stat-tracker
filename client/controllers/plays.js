1dd3969d460245c841e7a75bac7e30f31589691f
oji <dave@startrekstud.com>
2017-03-25 14:44:31 -0700

    test

--- client/controllers/plays.js
+++ client/controllers/plays.js
@@ -28,6 +28,13 @@ myApp.controller('PlaysController', ['$scope', '$filter', '$http', '$location', 
         return playerService.getSetPlayers();
     }
 
+    function filterPlayersByTeam($scope) {
+        $scope.playerByTeam= '';
+        $scope.newValue = function(playerByTeam) {
+           console.log(playerByTeam);
+        }
+    }
+
     $scope.players = null;
     $scope.playerList = [];
 
